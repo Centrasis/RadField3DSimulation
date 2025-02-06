c205d07ee649f60995392c4364a6cc3b10446446
Felix Lehner <lehner04@pad.ptb.de>
2025-02-06 15:52:36 +0100

    adding documentation

--- tools/create_dataset.py
+++ tools/create_dataset.py
@@ -393,7 +393,7 @@ if __name__ == "__main__":
         preexisting_samples = [int(f.removesuffix(".rf3")) for f in preexisting_samples]
     except:
         getLogger().warning("Could not parse preexisting samples! Was not a standard dataset naming convention.")
-    preexisting_samples_max_nb = max(preexisting_samples) if len(preexisting_samples) > 0 else 0
+    preexisting_samples_max_nb = (max(preexisting_samples) + 1) if len(preexisting_samples) > 0 else 0
 
     if cluster_node_partition is not None:
         parameters = [p for p in parameters]
